5228b6cefd1d5e0feb48205bb177dcb1f0c25833
angelsanchezh <angelsanchezh@gmail.com>
2022-11-30 21:48:24 -0500

    agrege whitenoise

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,4 +154,6 @@ SIMPLE_JWT={
 
 }
 CORS_ALLOWED_ORIGINS= [ 'http://127.0.0.1:5500', 'https://www.google.com' ]
+
+STATIC_ROOT = BASE_DIR / 'archivos_estaticos'
 
