53124065902b18d60a21b5e9b139d4feee188f86
angelsanchezh <angelsanchezh@gmail.com>
2022-12-14 23:12:24 -0500

    usuraios con hash

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,11 +1,14 @@
 import { Usuario } from "../models/usuarioModel.js";
+import bcryptjs from "bcryptjs";
+
 
 export async function registro (req,res) {
     const data = req.body;
     console.log(data);
 
     try{ 
-        const nuevoUsuario = await Usuario.create(data);
+        const password = bcryptjs.hashSync(data.password,10);
+        const nuevoUsuario = await Usuario.create({...data, password});
         console.log(nuevoUsuario);
 
         res.status(201).json({
@@ -14,7 +17,30 @@ export async function registro (req,res) {
     } catch(error) {
         res.status(500).json({
             message:"error a crear usuario usuario",
+        }); 
+    }}
+
+    export async function login(req,res){
+        const data =req.body;
+        const usuarioEncontrado = await Usuario.findOne({email: data.email  });
+        if (!usuarioEncontrado){
+            return res.status(404).json({
+
+                message: "el susuario no existe",
         });
 
-        
-    }}
+    }
+    if (bcryptjs.compareSync(data.password,usuarioEncontrado.password)){
+        res.json({
+            message:"bienvenida",
+        });
+
+    }else{
+        res.json({
+            message:"error al ingresar la contrasena",
+        });
+
+    }
+    
+
+}
